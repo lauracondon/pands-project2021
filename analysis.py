36c9b1c5dfdbada3807d8d3bb4c6f60ad4837101
Laura <G00398298@gmit.ie>
2021-04-07 23:20:09 +0100

    initial commit analysis.py

---
+++ analysis.py
@@ -0,0 +1,9 @@
+# outputs a summary of each variable to a single text file, 
+# saves a histogram of each variable to png files 
+# outputs a scatter plot of each pair of variables
+# Author: Laura Condon
+
+import numpy as np            
+import pandas as pd  
+import matplotlib.plyplot as plt     
+
