9074371131802fec6b204e7248bc75a5dce4a050
Laura <G00398298@gmit.ie>
2021-04-08 00:58:20 +0100

    created column and row subsets of dataset

--- analysis.py
+++ analysis.py
@@ -7,10 +7,22 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-ir = pd.read_csv("iris.data", names=['sepal length', 'sepal width', 'petal length', 'petal width', 'class']) # names assigns column names to the dataset
-print(ir.head()) # prints the first five rows
-print(type(ir)) # checks type of the object
-print(ir.shape) # tells you how many rows and columns in set
-print(ir.columns) # prints names of columns
-print(ir.dtypes)
-print(ir.info())
+ir = pd.read_csv("iris.data", names=["sepal length", "sepal width", "petal length", "petal width", "class"]) # names assigns column names to the dataset
+# print(ir.head()) # prints the first five rows
+# print(type(ir)) # checks type of the object
+# print(ir.shape) # tells you how many rows and columns in set
+# print(ir.columns) # prints names of columns
+# print(ir.dtypes) # get the datatype of each column
+# print(ir.info()) # gets information about dataset including memory usage
+
+sepal_subset = ir[["sepal length", "sepal width"]] # create subset based on columns
+petal_subset = ir[["petal length", "petal width"]]
+#print(sepal_subset)
+#print(petal_subset)
+
+setosa = ir.iloc[0:50] # create subset based on rows
+#print(setosa)
+versicolor = ir.iloc[50:100]
+#print(versicolor)
+virginica = ir.iloc[100:150]
+#print(virginica)
