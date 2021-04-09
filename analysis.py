7f88a8f7f940df0008798c43e17922bee45cacaa
Laura <G00398298@gmit.ie>
2021-04-09 17:49:33 +0100

    changed variable name to iris, created more subsets and tested basic calculations

--- analysis.py
+++ analysis.py
@@ -7,22 +7,39 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-ir = pd.read_csv("iris.data", names=["sepal length", "sepal width", "petal length", "petal width", "class"]) # names assigns column names to the dataset
-# print(ir.head()) # prints the first five rows
+iris = pd.read_csv("iris.data", names=["sepal length", "sepal width", "petal length", "petal width", "class"]) # names assigns column names to the dataset
+# print(iris.head()) # prints the first five rows
 # print(type(ir)) # checks type of the object
-# print(ir.shape) # tells you how many rows and columns in set
-# print(ir.columns) # prints names of columns
-# print(ir.dtypes) # get the datatype of each column
-# print(ir.info()) # gets information about dataset including memory usage
+# print(iris.shape) # tells you how many rows and columns in set
+# print(iris.columns) # prints names of columns
+# print(iris.dtypes) # get the datatype of each column
+# print(iris.info()) # gets information about dataset including memory usage
 
-sepal_subset = ir[["sepal length", "sepal width"]] # create subset based on columns
-petal_subset = ir[["petal length", "petal width"]]
+sepal_length = iris[["sepal length"]] # create subset based on columns
+sepal_width = iris[["sepal width"]]
+petal_length = iris[["petal length"]]
+petal_width = iris[["petal width"]]
+sepal_subset = iris[["sepal length", "sepal width"]] 
+petal_subset = iris[["petal length", "petal width"]]
 #print(sepal_subset)
 #print(petal_subset)
+#print(sepal_length)
 
-setosa = ir.iloc[0:50] # create subset based on rows
+setosa = iris.iloc[0:50] # create subset based on rows to seperate each class of iris
 #print(setosa)
-versicolor = ir.iloc[50:100]
+versicolor = iris.iloc[50:100]
 #print(versicolor)
-virginica = ir.iloc[100:150]
+virginica = iris.iloc[100:150]
 #print(virginica)
+
+mean1 = iris["sepal length"].mean()
+#print(round(mean1,2))
+mean2 = iris["sepal width"].mean()
+#print(round(mean2,2))
+
+# what should a summary of each variable include? max, min, median? 
+# how best to display it in text file? 
+# the variables are sepal length, sepal width, petal length, petal height and class
+# http://makemeanalyst.com/basic-statistics-explore-your-data-cases-variables-types-of-variables/
+# https://blogs.helsinki.fi/quantitative-communication/data-gathering/what-is-data/
+# https://sphweb.bumc.bu.edu/otlt/mph-modules/bs/bs704_summarizingdata/bs704_summarizingdata_print.html
